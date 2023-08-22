47293f3ba1b2384a1a69f9604b71e73b7bea64cb
ganeshthatikayala555 <141236379+ganeshthatikayala555@users.noreply.github.com>
2023-08-22 14:57:48 +0530

    Add files via upload

--- multipl-inher.java
+++ multipl-inher.java
@@ -1,48 +1,29 @@
-// First parent class
-Interface Mammal {
-    void eat();
-
-    void sleep();
-}
-
-// Second parent class
-Interface Bird {
-    void eat();
-
-    void fly() ;
-}
-
-// Class representing a Bat inheriting from Mammal and Bird using interfaces
-class Bat implements Mammal, Bird {
-    @Override
-    public void eat() {
-        System.out.println("Bat is eating.");
+class Bat {
+public:
+    void eat() {
+        cout << "Bat is eating" << endl;
     }
+    void fly() {
+        cout << "Bat is flying" << endl;
+    }
+};
+class Parrot {
+public:
+    void eat() {
+        cout << "Parrot is eating" << endl;
+    }
+    void fly() {
+        cout << "Parrot is flying" << endl;
+    }
+};
 
-    @Override
-    public void sleep() {
-        System.out.println("Bat is sleeping.");
-    }
+class   Bird : public Bat, public Parrot {
+public:
+    // Bird inherits both Parrot and Bird
+};
 
-    @Override
-    public void fly() {
-        System.out.println("Bat is flying.");
-    }
-}
-class Parrot implements Bird{
-    @Override
-    public void eat(){
-        System.out.println("Parrot is eating.");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Bat bat = new Bat();
-        Parrot p=new Parrot();
-        bat.eat();   // Bat's implementation of eat()
-        p.eat();//parrots implementation of eat()
-        bat.sleep(); // Bat's implementation of sleep()
-        bat.fly();   // Bat's implementation of fly()
-    }
+int main() {
+    Bird b;
+    b.eat(); // Which version of eat() should be called?
+    return 0;
 }
